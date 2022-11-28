3e61863f3a7b5ec4355248f7c3472475b769e81e
stan <dusek.s03@htlwienwest.at>
2022-11-28 16:10:14 +0100

    Fixed Upstream Commit Check Changed Gradle Settings

--- scripts/upstreamCommit.sh
+++ scripts/upstreamCommit.sh
@@ -10,12 +10,12 @@
 set -e
 PS1="$"
 
-paper=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PaperMC/Paper/compare/$1...$2 | jq -r '.commits[] | "pufferfish-gg/Pufferfish@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"')
+pufferfish=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/pufferfish-gg/Pufferfish/compare/$1...$2 | jq -r '.commits[] | "pufferfish-gg/Pufferfish@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"')
 
 updated=""
 logsuffix=""
 if [ ! -z "pufferfish" ]; then
-    logsuffix="$logsuffix\n\nPufferfish Changes:\n$paper"
+    logsuffix="$logsuffix\n\nPufferfish Changes:\n$pufferfish"
     updated="Pufferfish"
 fi
 disclaimer="Upstream has released updates that appear to apply and compile correctly"
